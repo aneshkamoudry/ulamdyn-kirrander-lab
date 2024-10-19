3dbbc6ea53aacff35d49a383ef09fb1a18b6a4de
Max Pinheiro Jr <maxjr82@gmail.com>
2024-10-19 20:02:03 +0000

    chore(ci): add GitLab CI integration with version bump step, and release

---
+++ src/ulamdyn/version.py
@@ -0,0 +1,10 @@
+"""Script for version control."""
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+#
+VERSION = "1.0.0"
